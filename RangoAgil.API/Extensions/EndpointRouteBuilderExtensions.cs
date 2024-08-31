36b46be3943e4cbef869d1e104fa247e15f45416
danieldpalma <danielpalma232@gmail.com>
2024-08-31 14:02:47 -0300

    feat: added Authentication, Authorization and Identity to endpoints

--- RangoAgil.API/Extensions/EndpointRouteBuilderExtensions.cs
+++ RangoAgil.API/Extensions/EndpointRouteBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using RangoAgil.API.EndpointFilters;
+﻿using Microsoft.AspNetCore.Identity;
+using RangoAgil.API.EndpointFilters;
 using RangoAgil.API.EndpointHandlers;
 
 namespace RangoAgil.API.Extensions;
@@ -7,32 +8,55 @@ public static class EndpointRouteBuilderExtensions
 {
     public static void RegisterRangosEndpoints(this IEndpointRouteBuilder endpointRouteBuilder)
     {
-        var rangoEndpoints = endpointRouteBuilder.MapGroup("/rangos");
+        endpointRouteBuilder.MapGroup("/identity/").MapIdentityApi<IdentityUser>();
+
+        endpointRouteBuilder.MapGet("/pratos/{pratoId:int}", (int pratoId) => $"O prato {pratoId} é delicioso")
+            .WithOpenApi(operation =>
+            {
+                operation.Deprecated = true;
+                return operation;
+            })
+            .WithSummary("This endpoint is obsolete and will be deprecated in version 2 of this API")
+            .WithDescription("Please use other route of this API, beeing /rango/{rangoId}");
+
+        var rangoEndpoints = endpointRouteBuilder.MapGroup("/rangos")
+            .RequireAuthorization();
+
         var rangosWithIdEndpoints = rangoEndpoints.MapGroup("/{rangoId:int}");
 
         var rangosWithIdEndEndpointsAndLockFilter = endpointRouteBuilder.MapGroup("/rangos/{rangoId:int}")
+            .RequireAuthorization("RequireAdminFromBrazil")
+            .RequireAuthorization()
             .AddEndpointFilter(new RangoIsLockedFilter(5))
             .AddEndpointFilter(new RangoIsLockedFilter(6))
             .AddEndpointFilter(new RangoIsLockedFilter(11));
 
-        rangoEndpoints.MapGet("", RangosHandlers.GetRangosAsync);
+        rangoEndpoints.MapGet("", RangosHandlers.GetRangosAsync).WithSummary("This endpoint gets all Rangos.");
 
-        rangosWithIdEndpoints.MapGet("", RangosHandlers.GetRangoById).WithName("GetRangos");
+        rangosWithIdEndpoints.MapGet("", RangosHandlers.GetRangoById)
+            .WithName("GetRangos")
+            .AllowAnonymous()
+            .WithSummary("This endpoint get a Rango by its Id.");
 
         rangoEndpoints.MapPost("", RangosHandlers.CreateRangoAsync)
-            .AddEndpointFilter<ValidateAnnotationFilter>();
+            .AddEndpointFilter<ValidateAnnotationFilter>()
+            .WithSummary("This endpoint create a Rango.");
 
-        rangosWithIdEndEndpointsAndLockFilter.MapPut("", RangosHandlers.UpdateRangoAsync);
+        rangosWithIdEndEndpointsAndLockFilter.MapPut("", RangosHandlers.UpdateRangoAsync)
+            .WithSummary("This endpoint update a Rango by its Id.");
 
         rangosWithIdEndEndpointsAndLockFilter.MapDelete("", RangosHandlers.DeleteRangoAsync)
-            .AddEndpointFilter<LogNotFoundResponseFilter>();
+            .AddEndpointFilter<LogNotFoundResponseFilter>()
+            .WithSummary("This endpoint delete a Rango by its Id.");
     }
 
     public static void RegisterIngredientsEndpoints(this IEndpointRouteBuilder endpointRouteBuilder)
     {
-        var ingredientsEndpoint = endpointRouteBuilder.MapGroup("/rangos/{rangoId:int}/ingredients");
+        var ingredientsEndpoint = endpointRouteBuilder.MapGroup("/rangos/{rangoId:int}/ingredients")
+            .RequireAuthorization();
 
-        ingredientsEndpoint.MapGet("", IngredientsHandlers.GetIngredientsAsync);
+        ingredientsEndpoint.MapGet("", IngredientsHandlers.GetIngredientsAsync)
+            .WithSummary("This endpoint gets all Ingredients of a Rango by its Id.");;
 
         ingredientsEndpoint.MapPost("", () =>
         {
--- RangoAgil.API/Program.cs
+++ RangoAgil.API/Program.cs
@@ -1,7 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using RangoAgil.API.DbContexts;
 using RangoAgil.API.Extensions;
-using System.Net;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,11 +10,58 @@ builder.Services.AddDbContext<RangoDbContext>(
     options => options.UseSqlite(builder.Configuration["ConnectionStrings:RangoDbConStr"])
 );
 
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<RangoDbContext>();
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddProblemDetails();
 
+builder.Services.AddAuthentication().AddJwtBearer();
+builder.Services.AddAuthorization();
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("RequireAdminFromBrazil", (policy) =>
+    {
+        policy
+            .RequireRole("admin")
+            .RequireClaim("country", "Brazil");
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("TokenAuthRango", new()
+    {
+        Name = "Authorization",
+        Description = "Token based in Authentication and Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        In = ParameterLocation.Header
+    });
+    options.AddSecurityRequirement(new()
+    {
+        {
+            new()
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "TokenAuthRango"
+                }
+            },
+            new List<string>()
+
+        }
+    });
+});
+
 var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 if(app.Environment.IsProduction())
 {
@@ -30,6 +78,12 @@ if(app.Environment.IsProduction())
             });
     });
     */
+}
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.RegisterRangosEndpoints();
